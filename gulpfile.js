5706304c411b0802626bd2ea14640629f7361983
sexfat <bryant6831@gmail.com>
2022-03-15 16:12:14 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -13,3 +13,21 @@ function task_default(cb){
 }
 
 exports.default = task_default;
+
+
+//A 任務
+function task_a(cb){
+    console.log('a mission');
+    cb();
+ }
+// B 任務
+ function task_b(cb){
+    console.log('b mission');
+    cb();
+ }
+
+ //有順序
+ exports.async = series(task_a , task_b);
+ 
+ //同時執行任務
+ exports.sync = parallel(task_a , task_b);
