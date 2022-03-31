364aff7845a83c7e976a85f3b97d28aab25bdc4f
sexfat <bryant6831@gmail.com>
2022-03-31 14:27:30 +0800

    add gsap

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 import $ from 'jquery';
 $('body').css('background-color' , 'red');
 
+import { gsap } from 'gsap';
+gsap.to('.box' , {
+    x: 200,
+    y : 400, 
+    duration : 3      
+})
+
+
 var x = 30 , y = 20;
 var z = x + y;
 
