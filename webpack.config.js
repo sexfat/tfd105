9494fdabe24b6ce630796f208d576f674b10ef8c
sexfat <bryant6831@gmail.com>
2022-03-31 16:17:46 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
 
 module.exports = {
-    entry: './script.js',               // 入口文件
+    entry: {index :'./script.js'},               // 入口文件
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
@@ -17,8 +20,18 @@ module.exports = {
             ],
         }]
 
-    },               // 處裡對應模組
-    // plugins: [],             // 對應的插件
+    },              // 處裡對應模組
+    plugins: [
+        new HtmlWebpackPlugin({
+            chunks : ['index'],  //選擇注入資源 chunk
+            inject  : 'body', //預設<body> js </body>  head or body
+            template : './src/index.html',
+            //來源
+            filename : 'index.html'
+            // 目的地
+        })
+
+    ],            // 對應的插件
     // devServer: {},           // 服務器配置
     mode: 'development'      // 開發模式配置
 }
